e949eea4e0fd846b6cdabb2d4681ba1d3c01672e
Cássio Pieroni <cassiopieroni@gmail.com>
2020-07-25 00:54:19 -0300

    Atualizando repositório

--- src/store/fetchActions/fetchBuffetData.js
+++ src/store/fetchActions/fetchBuffetData.js
@@ -1,7 +1,7 @@
 import { updateBuffetData } from "../ducks/initialData"
 import buffetData from "../../data/buffetData"
 
-//ESTE ARQUIVO APENAS SIMULA UMA BUSCA DE DADOS.
+//ESTE ARQUIVO APENAS SIMULA UMA BUSCA DE DADOS
 export const fetchBuffetData = () => {
 	return dispatch => {
 		dispatch(updateBuffetData(buffetData))
