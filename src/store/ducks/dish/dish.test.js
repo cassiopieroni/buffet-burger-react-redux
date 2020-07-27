0be652f6be2dbae510543a97cc8d5ab47d634115
Cássio Pieroni <cassiopieroni@gmail.com>
2020-07-27 17:31:26 -0300

    refatorando testes em dishReducer (store/ducks/dish)

--- src/store/ducks/dish/dish.test.js
+++ src/store/ducks/dish/dish.test.js
@@ -2,111 +2,75 @@ import dishReducer, { actions, INITIAL_STATE } from "./index"
 
 describe("dish reducer", () => {
 	describe(`Lidando com a action '${actions.TOGGLE_ITEM}'`, () => {
-		const itemA = {
+		const itemTypeBurgerValueA = {
 			price: 15,
 			type: "burger",
-			value: "AAA",
+			value: "A",
 		}
 
-		const itemB = {
+		const itemTypeBurgerValueB = {
 			price: 11,
 			type: "burger",
-			value: "BBB",
+			value: "B",
 		}
 
-		const itemC = {
+		const itemTypeCheeseValueA = {
 			price: 4,
 			type: "cheese",
-			value: "AAA",
+			value: "A",
 		}
 
-		const stateWithItemB = {
+		const stateWithAnItemTypeBurgerValueB = {
 			...INITIAL_STATE,
 			selectedItems: {
 				...INITIAL_STATE.selectedItems,
-				[itemB.type]: itemB,
+				[itemTypeBurgerValueB.type]: itemTypeBurgerValueB,
 			},
-			summaryItems: [{ ...itemB }],
-			subtotal: itemB.subtotal,
+			summaryItems: [{ ...itemTypeBurgerValueB }],
+			subtotal: itemTypeBurgerValueB.subtotal,
 		}
 
 		it(`Se 'payload.value' for diferente de 'state.selectedItems[payload.type].value', 
         deve atualizar o estado com 'payload'`, () => {
-			expect(
-				dishReducer(stateWithItemB, {
-					type: actions.TOGGLE_ITEM,
-					payload: itemA,
-				}).selectedItems[itemA.type].value
-			).toBe(itemA.value)
+			const newDishState = dishReducer(stateWithAnItemTypeBurgerValueB, {
+				type: actions.TOGGLE_ITEM,
+				payload: itemTypeBurgerValueA,
+			})
 
-			expect(
-				dishReducer(stateWithItemB, {
-					type: actions.TOGGLE_ITEM,
-					payload: itemA,
-				}).summaryItems
-			).toContain(itemA)
-
-			expect(
-				dishReducer(stateWithItemB, {
-					type: actions.TOGGLE_ITEM,
-					payload: itemA,
-				}).subtotal
-			).toBe(itemA.price)
+			expect(newDishState.selectedItems).toEqual({
+				...stateWithAnItemTypeBurgerValueB.selectedItems,
+				[itemTypeBurgerValueA.type]: itemTypeBurgerValueA,
+			})
+			expect(newDishState.summaryItems).toContain(itemTypeBurgerValueA)
+			expect(newDishState.summaryItems).not.toContain(itemTypeBurgerValueB)
+			expect(newDishState.subtotal).toBe(itemTypeBurgerValueA.price)
 		})
 
 		it(`Se 'payload.value' for igual a 'state.selectedItems[payload.type].value', 
         deve retornar 'state.selectedItems[payload.type].value = ""', retirando este item do estado`, () => {
-			expect(
-				dishReducer(stateWithItemB, {
-					type: actions.TOGGLE_ITEM,
-					payload: itemB,
-				}).selectedItems[itemB.type].value
-			).toBe("")
+			const newDishState = dishReducer(stateWithAnItemTypeBurgerValueB, {
+				type: actions.TOGGLE_ITEM,
+				payload: itemTypeBurgerValueB,
+			})
 
-			expect(
-				dishReducer(stateWithItemB, {
-					type: actions.TOGGLE_ITEM,
-					payload: itemB,
-				}).summaryItems
-			).not.toContain(itemB)
-
-			expect(
-				dishReducer(stateWithItemB, {
-					type: actions.TOGGLE_ITEM,
-					payload: itemB,
-				}).subtotal
-			).toBe(0)
+			expect(newDishState.selectedItems[itemTypeBurgerValueB.type].value).toBe("")
+			expect(newDishState.summaryItems).not.toContain(itemTypeBurgerValueB)
+			expect(newDishState.subtotal).toBe(0)
 		})
 
 		it(`Quando o estado já possuir valores e, se 'payload.type' for diferente desses valores presentes,
         deve atualizar o estado acrescentando 'playload' `, () => {
-			expect(
-				dishReducer(stateWithItemB, {
-					type: actions.TOGGLE_ITEM,
-					payload: itemC,
-				}).selectedItems[itemC.type].value
-			).toBe("AAA")
+			const newDishState = dishReducer(stateWithAnItemTypeBurgerValueB, {
+				type: actions.TOGGLE_ITEM,
+				payload: itemTypeCheeseValueA,
+			})
 
-			expect(
-				dishReducer(stateWithItemB, {
-					type: actions.TOGGLE_ITEM,
-					payload: itemC,
-				}).summaryItems
-			).toContain(itemB)
-
-			expect(
-				dishReducer(stateWithItemB, {
-					type: actions.TOGGLE_ITEM,
-					payload: itemC,
-				}).summaryItems
-			).toContain(itemB)
-
-			expect(
-				dishReducer(stateWithItemB, {
-					type: actions.TOGGLE_ITEM,
-					payload: itemC,
-				}).subtotal
-			).toBe(15)
+			expect(newDishState.selectedItems).toEqual({
+				...stateWithAnItemTypeBurgerValueB.selectedItems,
+				[itemTypeCheeseValueA.type]: itemTypeCheeseValueA,
+			})
+			expect(newDishState.summaryItems).toContain(itemTypeBurgerValueB)
+			expect(newDishState.summaryItems).toContain(itemTypeCheeseValueA)
 		})
 	})
 
@@ -119,26 +83,14 @@ describe("dish reducer", () => {
 
 		it(`Se o novo item extra selecionado (payload) não estiver presente em 'state.selectedItems.extras', 
         deve atualizar o estado com o novo item extra selecionado (payload)`, () => {
-			expect(
-				dishReducer(undefined, {
-					type: actions.TOGGLE_EXTRA_ITEM,
-					payload: extraItem,
-				}).selectedItems.extras
-			).toContain(extraItem)
+			const newDishState = dishReducer(undefined, {
+				type: actions.TOGGLE_EXTRA_ITEM,
+				payload: extraItem,
+			})
 
-			expect(
-				dishReducer(undefined, {
-					type: actions.TOGGLE_EXTRA_ITEM,
-					payload: extraItem,
-				}).summaryItems
-			).toContain(extraItem)
-
-			expect(
-				dishReducer(undefined, {
-					type: actions.TOGGLE_EXTRA_ITEM,
-					payload: extraItem,
-				}).subtotal
-			).toBe(extraItem.price)
+			expect(newDishState.selectedItems.extras).toContain(extraItem)
+			expect(newDishState.summaryItems).toContain(extraItem)
+			expect(newDishState.subtotal).toBe(extraItem.price)
 		})
 
 		it(`Se o novo item extra selecionado (payload) estiver presente em 'state.selectedItems.extras', 
@@ -153,26 +105,14 @@ describe("dish reducer", () => {
 				subtoal: extraItem.price,
 			}
 
-			expect(
-				dishReducer(stateWithExtraItem, {
-					type: actions.TOGGLE_EXTRA_ITEM,
-					payload: extraItem,
-				}).selectedItems.extras
-			).not.toContain(extraItem)
+			const newDishState = dishReducer(stateWithExtraItem, {
+				type: actions.TOGGLE_EXTRA_ITEM,
+				payload: extraItem,
+			})
 
-			expect(
-				dishReducer(stateWithExtraItem, {
-					type: actions.TOGGLE_EXTRA_ITEM,
-					payload: extraItem,
-				}).summaryItems
-			).not.toContain(extraItem)
-
-			expect(
-				dishReducer(stateWithExtraItem, {
-					type: actions.TOGGLE_EXTRA_ITEM,
-					payload: extraItem,
-				}).subtotal
-			).toBe(0)
+			expect(newDishState.selectedItems.extras).not.toContain(extraItem)
+			expect(newDishState.summaryItems).not.toContain(extraItem)
+			expect(newDishState.subtotal).toBe(0)
 		})
 	})
 })
